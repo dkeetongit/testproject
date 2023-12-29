b341d5143378e24a399f6f27a5725af2d968db24
dkeetongit <43971654+dkeetongit@users.noreply.github.com>
2023-12-29 00:08:21 -0500

    added tailwind

--- components/nav.vue
+++ components/nav.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
+  <nav class="flex gap-4 p-4 w-full border-b border-black">
     <a href="page1"> page 1 </a>
     <a href="/"> Home </a>
-  </div>
+  </nav>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.{html,js,ts,jsx,tsx,vue}",
+    "./node_modules/vue-tailwind/src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
